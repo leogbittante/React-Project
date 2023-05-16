4eb45af486784d9507ec9e1bd6e692fa83ee9b3d
Leonardo <62109173+leogbittante@users.noreply.github.com>
2023-05-16 13:46:55 -0300

    Update index.js

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -4,13 +4,13 @@ const ListaSuspensa = (props) => {
     return (
         <div>
             <label>{props.label}</label>
-            <select>
-                {props.itens.map(item => {
-                    return <option>(item)</option>
+            <select onchange={evento => props.aoAlterado(evento.target.value)} required={props.obrigatorio} value={props.valor}>
+                {props.itens.map((item, index) => {
+                    return <option key={index}>(item)</option>
                 })}
             </select>
         </div>
-    )
+    )   
 }
 
 export default ListaSuspensa
